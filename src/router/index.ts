0ae0d9416389e487da39b6a2b5129d8730003551
Anton Berestov <8berestov8@gmail.com>
2022-12-22 13:28:08 +0100

    rename footer tabs

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,39 +1,39 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router';
 import { RouteRecordRaw } from 'vue-router';
-import TabsPage from '../views/TabsPage.vue'
+import FooterTabs from '../views/FooterTabs.vue';
 
 const routes: Array<RouteRecordRaw> = [
-  {
-    path: '/',
-    redirect: '/tabs/tab1'
-  },
-  {
-    path: '/tabs/',
-    component: TabsPage,
-    children: [
-      {
-        path: '',
-        redirect: '/tabs/tab1'
-      },
-      {
-        path: 'tab1',
-        component: () => import('@/views/Tab1Page.vue')
-      },
-      {
-        path: 'tab2',
-        component: () => import('@/views/Tab2Page.vue')
-      },
-      {
-        path: 'tab3',
-        component: () => import('@/views/Tab3Page.vue')
-      }
-    ]
-  }
-]
+	{
+		path: '/',
+		redirect: '/tabs/home',
+	},
+	{
+		path: '/tabs/',
+		component: FooterTabs,
+		children: [
+			{
+				path: '',
+				redirect: '/tabs/home',
+			},
+			{
+				path: 'home',
+				component: () => import('@/views/Home.vue'),
+			},
+			{
+				path: 'catalog',
+				component: () => import('@/views/Catalog.vue'),
+			},
+			{
+				path: 'recording',
+				component: () => import('@/views/Recording.vue'),
+			},
+		],
+	},
+];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+	history: createWebHistory(process.env.BASE_URL),
+	routes,
+});
 
-export default router
+export default router;
---
+++ src/views/FooterTabs.vue
@@ -0,0 +1,67 @@
+<template>
+	<ion-page>
+		<ion-tabs>
+			<ion-router-outlet></ion-router-outlet>
+			<ion-tab-bar slot="bottom">
+				<ion-tab-button tab="home" href="/tabs/home">
+					<ion-icon :icon="triangle" />
+					<ion-label>Главная</ion-label>
+				</ion-tab-button>
+
+				<ion-tab-button tab="catalog" href="/tabs/catalog">
+					<ion-icon :icon="ellipse" />
+					<ion-label>Каталог</ion-label>
+				</ion-tab-button>
+
+				<ion-tab-button tab="recording" href="/tabs/recording">
+					<ion-icon :icon="square" />
+					<ion-label>Запись</ion-label>
+				</ion-tab-button>
+
+				<ion-tab-button tab="basket" href="/tabs/basket">
+					<ion-icon :icon="square" />
+					<ion-label>Корзина</ion-label>
+				</ion-tab-button>
+
+				<ion-tab-button tab="profile" href="/tabs/profile">
+					<ion-icon :icon="square" />
+					<ion-label>Профиль</ion-label>
+				</ion-tab-button>
+			</ion-tab-bar>
+		</ion-tabs>
+	</ion-page>
+</template>
+
+<script lang="ts">
+	import { defineComponent } from 'vue';
+	import {
+		IonTabBar,
+		IonTabButton,
+		IonTabs,
+		IonLabel,
+		IonIcon,
+		IonPage,
+		IonRouterOutlet,
+	} from '@ionic/vue';
+	import { ellipse, square, triangle } from 'ionicons/icons';
+
+	export default defineComponent({
+		name: 'TabsPage',
+		components: {
+			IonLabel,
+			IonTabs,
+			IonTabBar,
+			IonTabButton,
+			IonIcon,
+			IonPage,
+			IonRouterOutlet,
+		},
+		setup() {
+			return {
+				ellipse,
+				square,
+				triangle,
+			};
+		},
+	});
+</script>
