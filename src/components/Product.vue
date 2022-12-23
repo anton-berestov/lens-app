7ec7fa3875d60da78ed312afcd294f4c616a08fa
Anton Berestov <8berestov8@gmail.com>
2022-12-23 01:26:16 +0100

    created component Product.vue, added platforms ios and android, run to device

---
+++ src/components/Product.vue
@@ -0,0 +1,77 @@
+<template>
+  <ion-card class="card">
+    <ion-row class="ion-justify-content-center ion-align-items-center">
+      <img src="assets/image/product.png" class="image"/>
+    </ion-row>
+    <ion-card-header class="header">
+      <ion-card-title class="title">{{ title }}</ion-card-title>
+      <ion-card-subtitle class="price">{{ price }}</ion-card-subtitle>
+    </ion-card-header>
+  </ion-card>
+
+</template>
+
+<script>
+import {defineComponent} from "vue";
+import {IonCard, IonCardHeader, IonCardTitle, IonRow} from "@ionic/vue";
+
+export default defineComponent({
+  name: "Product",
+  props: {
+    title: {
+      type: String,
+      default: ''
+    },
+    price: {
+      type: String,
+      default: ''
+    },
+    oldPrice: {
+      type: String,
+      default: ''
+    },
+    img: {
+      type: String,
+      default: ''
+    },
+    sale: {
+      type: String,
+      default: ''
+    }
+  },
+  components: {
+    IonCard,
+    IonCardHeader,
+    IonCardTitle,
+    IonRow
+  }
+})
+</script>
+
+<style scoped lang="scss">
+.card {
+  width: 150px;
+  height: 170px;
+
+  .image {
+    width: 100%;
+    margin: 10px;
+  }
+
+  .header {
+    padding: 10px 15px;
+
+    .title {
+      font-size: 12px;
+      font-weight: 400;
+    }
+
+    .price {
+      color: #000000;
+      font-size: 14px;
+      font-weight: 600;;
+    }
+  }
+}
+
+</style>
--- src/views/Catalog.vue
+++ src/views/Catalog.vue
@@ -2,6 +2,14 @@
   <ion-page>
     <Header title="Каталог"/>
     <ion-content :fullscreen="true">
+      <div class="wrapper">
+        <Product title="Biofinity toric (3 шт)" price="1 200 ₽"/>
+        <Product title="Biofinity toric (6 шт)" price="2 400 ₽"/>
+        <Product title="Biofinity toric (3 шт)" price="1 200 ₽" old-price="1 500 ₽"/>
+        <Product title="Acuvue (6 шт)" price="2 600 ₽"/>
+        <Product title="Biofinity toric (30 шт)" price="5 100 ₽"/>
+        <Product title="Biofinity toric (3 шт)" price="1200 ₽"/>
+      </div>
 
     </ion-content>
   </ion-page>
@@ -14,13 +22,23 @@ import {
   IonContent,
 } from '@ionic/vue';
 import Header from "@/components/Header.vue";
+import Product from "@/components/Product.vue";
 
 export default defineComponent({
   name: 'Catalog',
   components: {
+    Product,
     Header,
     IonContent,
     IonPage,
   },
 });
 </script>
+
+<style scoped lang="scss">
+.wrapper {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+</style>
