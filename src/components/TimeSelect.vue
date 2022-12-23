88c1a3a7c3e0098c94d03a51713cc00467f594b9
Artur Voll <artur.voll@gmail.com>
2022-12-23 19:16:46 +0100

    - page Recording.vue with custom TimeSelect Component

---
+++ src/components/TimeSelect.vue
@@ -0,0 +1,87 @@
+<template>
+
+  <ion-grid id='time-select-wrapper'>
+    <ion-row>
+      <ion-col class='time-item' v-for='item in timeItems' :key='item.time' :disabled='!item.isFree'>
+        {{ item.time }}
+      </ion-col>
+    </ion-row>
+  </ion-grid>
+  <ion-card>
+  </ion-card>
+</template>
+
+<script lang='ts'>
+import { defineComponent } from 'vue';
+import { IonCard, IonGrid, IonRow, IonCol } from '@ionic/vue';
+
+export default defineComponent({
+  name: 'TimeSelect',
+  components: {
+    IonCard, IonGrid, IonRow, IonCol
+  },
+  data() {
+    return {
+      timeItems: [
+        { time: '10:00', isFree: true },
+        { time: '11:00', isFree: false },
+        { time: '12:00', isFree: true },
+        { time: '13:00', isFree: true },
+        { time: '14:00', isFree: true },
+        { time: '15:00', isFree: true },
+        { time: '16:00', isFree: true },
+        { time: '17:00', isFree: false },
+        { time: '18:00', isFree: true },
+        { time: '19:00', isFree: false }
+      ]
+    };
+  }
+});
+</script>
+
+<style lang='scss' scoped>
+
+#time-select-wrapper {
+
+  .time-item {
+    /* Component 5 */
+
+    color: var(--body-color);
+    box-sizing: border-box;
+
+    /* Auto layout */
+
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    padding: 5px 10px;
+    gap: 10px;
+
+    width: 50px;
+    height: 25px;
+
+    background: var(--body-bg);
+    border: 1px solid #E7E7E7;
+    border-radius: 2px;
+
+    /* Inside auto layout */
+
+    flex: none;
+    order: 0;
+    flex-grow: 0;
+  }
+
+}
+
+/*ion-col {
+  padding: 0;
+  margin: 0;
+
+  ion-item {
+    padding: 5px 10px;
+    margin: 0;
+    font-size: 10px;
+  }
+}*/
+</style>
--- src/views/Recording.vue
+++ src/views/Recording.vue
@@ -1,21 +1,132 @@
 <template>
   <ion-page>
-    <Header title="Запись" />
-    <ion-content :fullscreen="true"> </ion-content>
+    <Header title='Запись к врачу' custom contact back />
+    <ion-content :fullscreen='true'>
+      <ion-item-group>
+        <ion-item-divider>
+          <ion-label>Выберите дату</ion-label>
+        </ion-item-divider>
+        <ion-datetime></ion-datetime>
+      </ion-item-group>
+      <ion-item-group>
+        <ion-item-divider>
+          <ion-label>Выберите время</ion-label>
+        </ion-item-divider>
+
+        <TimeSelect></TimeSelect>
+
+      </ion-item-group>
+      <ion-item-group id='service'>
+        <ion-item-divider>
+          <ion-label>Выберите услугу</ion-label>
+        </ion-item-divider>
+        <ion-grid>
+          <ion-row>
+            <ion-col size='6'>
+              <ion-button>Подбор очковой коррекции</ion-button>
+            </ion-col>
+            <ion-col size='6'>
+              <ion-button fill='outline'>Подбор контактной коррекции</ion-button>
+            </ion-col>
+          </ion-row>
+        </ion-grid>
+
+      </ion-item-group>
+      <Button title='Продолжить' id='next-step'></Button>
+    </ion-content>
   </ion-page>
 </template>
 
-<script lang="ts">
+<script lang='ts'>
 import { defineComponent } from 'vue';
-import { IonPage, IonContent } from '@ionic/vue';
+import {
+  IonPage,
+  IonContent,
+  IonItemGroup,
+  IonDatetime,
+  IonItemDivider,
+  IonLabel,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonButton
+} from '@ionic/vue';
 import Header from '@/components/Header.vue';
+import TimeSelect from '@/components/TimeSelect.vue';
+import Button from '@/components/Button.vue';
 
 export default defineComponent({
   name: 'Recording',
   components: {
+    Button,
+    TimeSelect,
     IonContent,
     IonPage,
     Header,
-  },
+    IonItemGroup,
+    IonItemDivider,
+    IonLabel,
+    IonDatetime,
+    IonGrid,
+    IonRow,
+    IonCol,
+    IonButton
+  }
 });
 </script>
+
+<style lang='scss'>
+
+ion-item-divider {
+  background: transparent;
+  margin: 16px 0 12px 0;
+}
+
+ion-label {
+  color: var(--ion-color-dark-contrast);
+}
+
+
+#service {
+
+  ion-col {
+
+    ion-button {
+      width: 100%;
+      white-space: pre-wrap;
+      font-size: 12px;
+
+      --padding-start: auto;
+      --padding-end: auto;
+
+      --padding-top: 5px;
+      --padding-bottom: 5px;
+    }
+
+    &:first-child {
+      margin-right: 0;
+      padding-right: 0;
+
+      ion-button {
+        --border-radius: 5px 0 0 5px;
+      }
+    }
+
+    &:last-child {
+      margin-left: 0;
+      padding-left: 0;
+
+      ion-button {
+        --border-radius: 0 5px 5px 0;
+      }
+    }
+  }
+
+  margin-bottom: 34px;
+}
+
+#next-step {
+  width: 98%;
+}
+
+</style>
